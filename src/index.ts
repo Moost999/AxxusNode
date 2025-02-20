36c78245c1c2c1621167188911098322872ca9c6
Moost999 <geertschaffer@gmail.com>
2025-02-20 11:09:24 -0300

    cors options

--- src/index.ts
+++ src/index.ts
@@ -50,8 +50,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
-  res.header('Vary', 'Origin');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
   next();
 });
