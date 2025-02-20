5b6c4fd3dea82d9a7fb9ac6f3f5046398c923b60
Moost999 <geertschaffer@gmail.com>
2025-02-20 11:13:18 -0300

    arrray

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ app.options('*', cors(corsOptions)); // Habilitar preflight para todas as rotas
 app.use((req: Request, res: Response, next: NextFunction) => {
   const origin = req.headers.origin || '';
   if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', allowedOrigins[2]);
   }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
