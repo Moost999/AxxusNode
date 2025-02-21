0b8028026069671f5496d33711f1e4b1cbb270bb
Moost999 <geertschaffer@gmail.com>
2025-02-21 20:03:27 -0300

    add netfliy deploy

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ const allowedOrigins = [
   'https://axxus-front.vercel.app',
   'https://axxus-front-git-main-axxus.vercel.app',
   'http://localhost:3000',
+  'https://axxus.netlify.app',
   'http://192.168.0.2:3000'
 ];
 
